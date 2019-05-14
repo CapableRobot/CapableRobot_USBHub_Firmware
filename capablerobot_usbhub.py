4bb25cc74e1dbdd55699e06f923f727e1709c5aa
Chris Osterwood <osterwood@gmail.com>
2019-05-14 08:45:25 -0400

    Added MIT License text to top of source files

--- capablerobot_usbhub.py
+++ capablerobot_usbhub.py
@@ -1,3 +1,25 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2019 Chris Osterwood for Capable Robot Components
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 import board
 import digitalio
 import time
@@ -165,7 +187,7 @@ class USBHub:
         ## Upstream port is not in the speed register, so take data from
         ## the connection register.  Unfortunately, no way to know speed.
         out[0] = (conn & 0b1)*3
-        
+
         return out
 
     def attach(self):
