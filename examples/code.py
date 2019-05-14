76ff8f7f09014da1711fac5a8bdb614674524f3f
Chris Osterwood <osterwood@gmail.com>
2019-05-13 23:31:38 -0400

    Change data LEDs based on connected device speed

--- examples/code.py
+++ examples/code.py
@@ -5,6 +5,9 @@ import supervisor
 import time
 
 import capablerobot_usbhub
+import capablerobot_tlc59116
+
+boot_time = time.monotonic()
 
 led1 = digitalio.DigitalInOut(board.LED1)
 led1.switch_to_output(value=False)
@@ -21,21 +24,39 @@ def stdout(*args):
     if supervisor.runtime.serial_connected:
         print(*args)
 
-stdout("... booted     ...")
+stdout("... booted ...")
 
+i2c1 = busio.I2C(board.SCL,  board.SDA)
 i2c2 = busio.I2C(board.SCL2, board.SDA2)
+
+stdout("... configuring hub ...")
 usb = capablerobot_usbhub.USBHub(i2c2)
 
-stdout("... configured ...")
 
-stdout("ID ", usb.id)
-stdout("VENDOR_ID ", usb.vendor_id)
-stdout("PRODUCT_ID ", usb.product_id)
+stdout("... configuring leds ...")
+BRIGHT   = 20
+led_pwr  = capablerobot_tlc59116.TLC59116(i2c1, 0x61, pwm=BRIGHT)
+led_data = capablerobot_tlc59116.TLC59116(i2c1, 0x62, pwm=BRIGHT)
+
 
 while True:
-    
-    led3.value = False
-    time.sleep(BEAT)
+    time.sleep(0.1)
 
-    led3.value = True
-    time.sleep(1-BEAT)
+    led3.value = not led3.value
+
+    print(time.monotonic() - boot_time, usb.speeds)
+
+    ## Set the data leds based on the detected per-port speeds
+    for idx, speed in enumerate(usb.speeds):
+        color = (0,0,0)
+
+        if speed == 0b01:
+            color = (0,0,255)
+        if speed == 0b10:
+            color = (0,255,0)
+        if speed == 0b11:
+            color = (255,0,0)
+
+        led_data.rgb(idx, color, update=False)
+
+    led_data.update()
