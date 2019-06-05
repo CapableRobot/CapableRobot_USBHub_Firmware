1800bf1308ce75f50a3667e93d8780ef7b07ff04
Chris Osterwood <osterwood@gmail.com>
2019-06-04 22:01:56 -0400

    Reduce data LED brightness.  Power LEDs now light based on UCS2113 information

--- examples/code.py
+++ examples/code.py
@@ -9,6 +9,7 @@ import supervisor
 import capablerobot_usbhub
 import capablerobot_tlc59116
 import capablerobot_eeprom
+import capablerobot_ucs2113
 
 boot_time = time.monotonic()
 
@@ -22,6 +23,7 @@ led3 = digitalio.DigitalInOut(board.LED3)
 led3.switch_to_output(value=True)
 
 BEAT = 0.05
+LED_BRIGHT = 80
 
 def stdout(*args):
     if supervisor.runtime.serial_connected:
@@ -34,6 +36,7 @@ i2c2 = busio.I2C(board.SCL2, board.SDA2)
 
 stdout("... configuring hub ...")
 usb = capablerobot_usbhub.USBHub(i2c2)
+ucs = capablerobot_ucs2113.Ports(i2c1)
 
 stdout("... configuring leds ...")
 BRIGHT   = 20
@@ -56,16 +59,37 @@ while True:
         color = (0,0,0)
 
         if speed == 0b01:
-            color = (0,0,255)
+            color = (0,0,LED_BRIGHT)
         if speed == 0b10:
-            color = (0,255,0)
+            color = (0,LED_BRIGHT,0)
         if speed == 0b11:
-            color = (255,255,255)
+            color = (LED_BRIGHT,LED_BRIGHT,LED_BRIGHT)
 
         ## If the upstream port is disconnected, light the LED reg
         if speed == 0b00 and idx == 0:
-            color = (255,0,0)
+            color = (LED_BRIGHT,0,0)
 
         led_data.rgb(idx, color, update=False)
 
     led_data.update()
+
+    ## Set the power leds based on the measured per-port current
+    for idx, current in enumerate(ucs.currents(raw=True, rescale=2)):
+
+        ## With rescaling, raw reading may be above 255 (max value for LED), so cap it
+        if current > 255:
+            current == 255
+
+        if idx == 0:
+            color = (0,0,int(current/4))
+        else:
+            color = (0,0,current)
+
+        led_pwr.rgb(idx, color, update=False)
+
+    led_pwr.update()
+
+
+
+
+
