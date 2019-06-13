be4485139a6f08c7232c088236a09ef042800e60
Chris Osterwood <osterwood@gmail.com>
2019-06-12 21:38:21 -0400

    Power and Data LEDs now light orange if that port & function have been disabled

--- examples/code.py
+++ examples/code.py
@@ -35,7 +35,7 @@ i2c1 = busio.I2C(board.SCL,  board.SDA)
 i2c2 = busio.I2C(board.SCL2, board.SDA2)
 
 stdout("... configuring hub ...")
-usb = capablerobot_usbhub.USBHub(i2c2)
+usb = capablerobot_usbhub.USBHub(i2c1, i2c2)
 ucs = capablerobot_ucs2113.Ports(i2c1)
 
 stdout("... configuring leds ...")
@@ -49,29 +49,35 @@ stdout("serial number: %s" % eeprom.eui)
 while True:
     time.sleep(0.1)
 
+    ## Internal heartbeat LED
     led3.value = not led3.value
 
-    # stdout(time.monotonic() - boot_time, usb.speeds)
-    # stdout(time.monotonic() - boot_time, usb.rails)
+    data_state = usb.data_state()
 
     ## Set the data leds based on the detected per-port speeds
     for idx, speed in enumerate(usb.speeds):
         color = (0,0,0)
 
-        if speed == 0b01:
+        if idx > 0 and data_state[idx-1] == False:
+            ## If port data is disabled, light the LED orange
+            color = (LED_BRIGHT,int(LED_BRIGHT/2),0)
+        elif speed == 0b01:
             color = (0,0,LED_BRIGHT)
-        if speed == 0b10:
+        elif speed == 0b10:
             color = (0,LED_BRIGHT,0)
-        if speed == 0b11:
+        elif speed == 0b11:
             color = (LED_BRIGHT,LED_BRIGHT,LED_BRIGHT)
 
-        ## If the upstream port is disconnected, light the LED reg
+        ## If the upstream port is disconnected, light the LED red
         if speed == 0b00 and idx == 0:
             color = (LED_BRIGHT,0,0)
 
         led_data.rgb(idx, color, update=False)
 
     led_data.update()
+
+
+    power_state = usb.power_state()
 
     ## Set the power leds based on the measured per-port current
     for idx, current in enumerate(ucs.currents(raw=True, rescale=2)):
@@ -83,7 +89,12 @@ while True:
         if idx == 0:
             color = (0,0,int(current/4))
         else:
-            color = (0,0,current)
+            if power_state[idx-1] == False:
+                ## If port power is disabled, light the LED orange
+                color = (LED_BRIGHT,int(LED_BRIGHT/2),0)
+            else:
+                ## Otherwise, light blue with intensity based on measured power draw
+                color = (0,0,current)
 
         led_pwr.rgb(idx, color, update=False)
 
